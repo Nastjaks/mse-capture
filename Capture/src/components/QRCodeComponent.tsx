7e827df74981df05937655472bb8201c8f7cdacb
Andi <andreas.kuessner@mni.thm.de>
2025-01-29 18:58:35 +0100

    updated links again

--- Capture/src/components/QRCodeComponent.tsx
+++ Capture/src/components/QRCodeComponent.tsx
@@ -13,11 +13,12 @@ interface QRCodeComponentProps {
 const QRCodeComponent: React.FC<QRCodeComponentProps> = ({galleryId, istShareOpen}) => {
     const [qrCodeData, setQrCodeData] = useState<string | null>(null);
     const {showToast} = useToast();
+    const joinGalleryUrl = "capture-mse.netlify.app/join-gallery/";
 
     useEffect(() => {
         const generateQRCode = async () => {
             try {
-                const qrCodeUrl = `capture-mse.netlify.app/${galleryId}`;
+                const qrCodeUrl = `${joinGalleryUrl}${galleryId}`;
                 const qrCode = await QRCode.toDataURL(qrCodeUrl);
                 setQrCodeData(qrCode);
             } catch (err) {
@@ -66,7 +67,7 @@ const QRCodeComponent: React.FC<QRCodeComponentProps> = ({galleryId, istShareOpe
                             <>
                                 <div className="action-modal-title qr-image-top">
                                     <p>Invite your friends</p>
-                                    <IonButton shape="round" onClick={() => copyToClipboard("capture-mse.netlify.app/join-gallery/" + galleryId)}> <IonIcon aria-hidden="true" icon={copySharp}/>Copy Link</IonButton>
+                                    <IonButton shape="round" onClick={() => copyToClipboard(joinGalleryUrl + galleryId)}> <IonIcon aria-hidden="true" icon={copySharp}/>Copy Link</IonButton>
                                 </div>
 
                                 <div className="qr-image-content">
